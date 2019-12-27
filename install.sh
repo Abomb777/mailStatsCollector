b332a981069963857c86499c113767987cdc2e68
Abomb777 <abomb82@gmail.com>
2019-12-27 23:49:05 +0200

    check if installed app

--- install.sh
+++ install.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 yum update -y
-pack="pflogsumm"
-isinstalled=$(rpm -q $pack)
-if [ ! "$isinstalled" == "package $pack is not installed" ]; then
-	echo "Package  $pack already installed"
-else
-	echo "$pack is not INSTALLED!!!!"
+
+check_for_program() {
+  local program 
+  program="${1}"
+
+#  printf "Checking for ${program}\\n  "
+  command -v "${program}"
+
+  if [[ "${?}" -ne 0 ]]; then
+    printf "${program} is not installed, exiting\\n"
 	yum install $pack -y
-fi
+#    exit 1
+  else 
+	printf "${program} is  installed!\\n"
+  fi 
+}
+
+check_for_program "pflogsumm"
+#pack="pflogsumm"
+#isinstalled=$(rpm -q $pack)
+#if [ ! "$isinstalled" == "package $pack is not installed" ]; then
+#	echo "Package  $pack already installed"
+#else
+#	echo "$pack is not INSTALLED!!!!"
+#	yum install $pack -y
+#fi
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
