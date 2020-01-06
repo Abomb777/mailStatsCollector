289a988f3bad755b8c923d3f8673bde9bdd0df1c
Abomb777 <abomb82@gmail.com>
2020-01-06 02:40:45 +0200

    if statistic not per days

--- stats.sh
+++ stats.sh
@@ -8,6 +8,25 @@ line=`cat /var/log/maillog | /usr/sbin/pflogsumm | sed -n '/^Per\-Day Traffic Su
 #echo `echo $line | awk -F: '{print $2}'`
 #echo "-$secline-"
 #exit 0
+
+if [ "$line" == "" ]; then
+#no statistic per day
+	IN=`cat /var/log/maillog | /usr/sbin/pflogsumm | sed -n '/^messages/,/bytes.*/{/bytes.*/d;p}' | tail -n +2`
+	lineparams=$(echo $IN | tr " " "\n")
+	counter=0
+
+	line=":"
+	line="$line`date +'%-b %-d %Y'`"
+	for addr in $lineparams
+	do
+			if [ "$counter" -eq 0 ] || [ "$counter" -eq 2 ] || [ "$counter" -eq 6 ] || [ "$counter" -eq 8 ] || [ "$counter" -eq 10 ] ; then
+					line="$line:$addr"
+			fi
+			counter=$((counter+1))
+	done
+	#echo $line
+fi
+
 #echo -e "1$secline\n2$line\n"
 echo -e "$secline\n$line\n" > $BASEDIR/stats.txt
 sh $BASEDIR/calculate.sh
